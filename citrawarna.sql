900e90799064ece85908f786d2e2fc066e3b7169
refojunior <30499443+refojunior@users.noreply.github.com>
2018-04-30 17:10:26 +0800

    Add files via upload

--- citrawarna.sql
+++ citrawarna.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: 26 Apr 2018 pada 07.13
+-- Generation Time: 30 Apr 2018 pada 11.10
 -- Versi Server: 10.1.10-MariaDB
 -- PHP Version: 7.0.3
 
@@ -45,9 +45,7 @@ CREATE TABLE `artikel` (
 --
 
 INSERT INTO `artikel` (`id_artikel`, `tanggal`, `judul`, `link`, `keyword`, `excerpt`, `isi`, `id_kategori`, `img`, `stat`, `click`) VALUES
-(1, '2018-04-24 12:25:46', 'Artikel Percobaan', 'artikel-percobaan', 'Artikel Percobaan gan', 'Artikel Percobaan gan Artikel Percobaan gan Artikel Percobaan gan', 'Artikel Percobaan gan Artikel Percobaan gan Artikel Percobaan gan Artikel Percobaan gan Artikel Percobaan gan Artikel Percobaan gan Artikel Percobaan gan Artikel Percobaan gan Artikel Percobaan gan Artikel Percobaan gan Artikel Percobaan gan Artikel Percobaan gan ', 1, 'null.jpg', 1, 1),
-(17, '2018-04-26 11:29:22', 'aaa bbb ccc ddd', 'aaa-bbb-ccc-ddd', 'aaa bbb', 'aaa', '<p>aaa</p>\r\n', 1, '1r.png', 1, 0),
-(18, '2018-04-26 12:07:29', 'Minami Hamabe', 'minami-hamabe', 'Minami Hamabe', 'Minami Hamabe', '<p>Minami Hamabe</p>\r\n', 2, '1.jpg', 1, 0);
+(1, '2018-04-27 08:52:05', 'Artikel Percobaan', 'artikel-percobaan', 'Artikel Percobaan gan', 'Artikel Percobaan gan Artikel Percobaan gan Artikel Percobaan gan', '<p>Artikel Percobaan gan Artikel Percobaan gan Artikel Percobaan gan Artikel Percobaan gan Artikel Percobaan gan Artikel Percobaan gan Artikel Percobaan gan Artikel Percobaan gan Artikel Percobaan gan Artikel Percobaan gan Artikel Percobaan gan Artikel Percobaan gan</p>\r\n', 1, 'null.jpg', 1, 1);
 
 -- --------------------------------------------------------
 
@@ -85,6 +83,62 @@ CREATE TABLE `login_log` (
 -- --------------------------------------------------------
 
 --
+-- Struktur dari tabel `raw`
+--
+
+CREATE TABLE `raw` (
+  `id` int(11) NOT NULL,
+  `nim` int(11) NOT NULL,
+  `nama` varchar(10) NOT NULL,
+  `jurusan` varchar(30) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `setting`
+--
+
+CREATE TABLE `setting` (
+  `param` varchar(30) NOT NULL,
+  `value` varchar(30) NOT NULL,
+  `description` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data untuk tabel `setting`
+--
+
+INSERT INTO `setting` (`param`, `value`, `description`) VALUES
+('last_raw_update', '2018-04-30 14:27:41', 'Waktu Terakhir RAW File diupdate'),
+('last_raw_upload', '2018-04-30 16:45:50', 'Waktu Terakhir RAW File diupload');
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `slideshow`
+--
+
+CREATE TABLE `slideshow` (
+  `id_slideshow` int(11) NOT NULL,
+  `tanggal` date NOT NULL,
+  `judul` varchar(50) NOT NULL,
+  `url` varchar(50) NOT NULL,
+  `img` varchar(100) NOT NULL,
+  `z-index` int(2) NOT NULL,
+  `stat` int(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data untuk tabel `slideshow`
+--
+
+INSERT INTO `slideshow` (`id_slideshow`, `tanggal`, `judul`, `url`, `img`, `z-index`, `stat`) VALUES
+(4, '2018-04-27', 'Coba gan', 'https://youtu.be/fguU8zWhDXk', 'Folder.jpg', 1, 1);
+
+-- --------------------------------------------------------
+
+--
 -- Struktur dari tabel `user`
 --
 
@@ -100,7 +154,8 @@ CREATE TABLE `user` (
 --
 
 INSERT INTO `user` (`id_user`, `username`, `password`, `stat`) VALUES
-(1, 'refo', '9009337cf16333f07109b593405cf7552ed8059a', 1);
+(1, 'refo', '9009337cf16333f07109b593405cf7552ed8059a', 1),
+(2, 'junior', '5a066c88550d05b7554d79f47a9d12c436e7e6ce', 1);
 
 --
 -- Indexes for dumped tables
@@ -125,6 +180,24 @@ ALTER TABLE `login_log`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Indexes for table `raw`
+--
+ALTER TABLE `raw`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `setting`
+--
+ALTER TABLE `setting`
+  ADD PRIMARY KEY (`param`);
+
+--
+-- Indexes for table `slideshow`
+--
+ALTER TABLE `slideshow`
+  ADD PRIMARY KEY (`id_slideshow`);
+
+--
 -- Indexes for table `user`
 --
 ALTER TABLE `user`
@@ -138,7 +211,7 @@ ALTER TABLE `user`
 -- AUTO_INCREMENT for table `artikel`
 --
 ALTER TABLE `artikel`
-  MODIFY `id_artikel` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=19;
+  MODIFY `id_artikel` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 --
 -- AUTO_INCREMENT for table `kategori`
 --
@@ -150,10 +223,20 @@ ALTER TABLE `kategori`
 ALTER TABLE `login_log`
   MODIFY `id` int(5) NOT NULL AUTO_INCREMENT;
 --
+-- AUTO_INCREMENT for table `raw`
+--
+ALTER TABLE `raw`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `slideshow`
+--
+ALTER TABLE `slideshow`
+  MODIFY `id_slideshow` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+--
 -- AUTO_INCREMENT for table `user`
 --
 ALTER TABLE `user`
-  MODIFY `id_user` int(5) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id_user` int(5) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
