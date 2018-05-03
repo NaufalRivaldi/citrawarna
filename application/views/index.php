8768b539fdce96852f2bb42d6f5df23616a0072f
refojunior <refojunior5@gmail.com>
2018-05-03 14:17:56 +0800

    frontend kacau

--- application/views/index.php
+++ application/views/index.php
@@ -95,40 +95,69 @@
 		<h1 align="center">About Us</h1>
 		<hr>
 		<div class="row">
-			<h5 class="about">Citra Warna adalah toko cat terbesar di Bali, yang menyediakan produk-produk berkualitas, dan menjamin harga termurah.
-			Cabang kami tersebar didaerah Denpasar, Nusadua, Ubud, Tabanan, Singaraja dan akan terus berkembang. 
-			Berikut adalah kelebihan berbelanja di Citra Warna yang tidak akan anda dapatkan di toko lain</h5>
+			<div class="col-md-6">
+				<h5 class="about">Citra Warna adalah toko cat terbesar di Bali, yang menyediakan produk-produk berkualitas, dan menjamin harga termurah.Cabang kami tersebar didaerah Denpasar, Nusadua, Ubud, Tabanan, Singaraja dan akan terus berkembang. </h5>
+				<hr>
+				<h5><b>Visi & Misi Perusahaan kami</b></h5>
+				<h5>Visi : </h5>
+				<ul>
+					<li><p>Membentuk karakter guna kesejahteraan bersama di PT. Citra Warna Jaya Abadi.</p></li>
+				</ul>
+				<h5>Misi : </h5>
+				<ol>
+					<li>Memberikan pelayanan dan edukasi pengecatan yang optimal kepada pelanggan</li>
+					<li>Memberikan pilihan produk yang berkualitas dengan pilihan yang lengkap</li>
+					<li>Memberikan harga yang kompetitif</li>
+				</ol>
 
-		</div>
-		<br>
-		<div class="row">
-			<div class="col-md-4">
-				<h4 align="center"><b>Jaminan Harga termurah</b></h4>
-				<img src="assets/img/logo-new.jpg" alt="" class="img-fluid">
 			</div>
-			<div class="col-md-4">
-				<h4 align="center"><b>Produk Terlengkap</b></h4>
-				<img src="assets/img/logo-new.jpg" alt="" class="img-fluid">
-			</div>
-			<div class="col-md-4">
-				<h4 align="center"><b>Banyak Pilihan Warna</b></h4>
-				<img src="assets/img/logo-new.jpg" alt="" class="img-fluid">
+			<div class="col-md-6">
+				<img src="<?= base_url('assets/img/about.png') ?>" alt="" class="img-fluid">
 			</div>
 		</div>
-		<br>
-		<div class="row">
-			<div class="col-md-4">
-				<h4 align="center"><b>Jasa Pengecekan Tembok</b></h4>
-				<img src="assets/img/logo-new.jpg" alt="" class="img-fluid">
+	</div>
+	<div class="container-fluid bg-warning">
+	<br>
+		<div class="container">	
+			<h1 align="center">Layanan kami </h1>
+			<hr>
+			
+			<div class="row">
+				<div class="col-md-4">
+					<h4 align="center"><b>Jaminan Harga termurah</b></h4>
+					<img src="assets/img/logo-new.jpg" alt="" class="img-fluid">
+					<p>Kami menjamin toko kami dengan harga yang paling murah, apabila ada yang lebih murah, kami ganti selisihnya. (syarat dan ketentuan berlaku)</p>
+				</div>
+				<div class="col-md-4">
+					<h4 align="center"><b>Produk Terlengkap</b></h4>
+					<img src="assets/img/features-1.png" alt="" class="img-fluid">
+					<p>Segala jenis cat dan keperluan pengecatan anda ada disini, mulai dari thinner, kuas, amplas dan sebagainya</p>
+				</div>
+				<div class="col-md-4">
+					<h4 align="center"><b>Banyak Pilihan Warna</b></h4>
+					<img src="assets/img/features-3.jpg" alt="" class="img-fluid">
+					<p>Warna apapun yang anda inginkan ada disini, karena kami juga memiliki mesin tinting untuk membuat cat dengan warna yang anda inginkan</p>
+				</div>
 			</div>
-			<div class="col-md-4">
-				<h4 align="center"><b>Melayani Pembelian Partai Besar / Kecil</b></h4>
-				<img src="assets/img/logo-new.jpg" alt="" class="img-fluid">
+			<br>
+			<div class="row">
+				<div class="col-md-4">
+					<h4 align="center"><b>Jasa Pengecekan Tembok</b></h4>
+					<img src="assets/img/features-4.png" alt="" class="img-fluid">
+					<p>Kami juga menyediakan layanan jasa pengecekan kesiapan atau kesehatan tembok sebelum melakukan pengecatan, GRATIS tidak dipungut biaya (Khusus area denpasar)</p>
+				</div>
+				<div class="col-md-4">
+					<h4 align="center"><b>Melayani Pembelian Partai Besar / Kecil</b></h4>
+					<img src="assets/img/logo-new.jpg" alt="" class="img-fluid">
+					<p>Pembelian dalam jumlah besar hingga lebih dari 10 pail, atau pembelian hanya 1 kaleng, kami akan tetap melayani anda dengan sebaik-baiknya</p>
+				</div>
+				<div class="col-md-4">
+					<h4 align="center"><b>Delivery Service</b></h4>
+					<img src="assets/img/logo-new.jpg" alt="" class="img-fluid">
+					<p>Melayani jasa pengiriman barang ke lokasi anda</p>
+				</div>
 			</div>
-			<div class="col-md-4">
-				<h4 align="center"><b>Delivery Service</b></h4>
-				<img src="assets/img/logo-new.jpg" alt="" class="img-fluid">
-			</div>
+		</div>
 		</div>
 	</div>
 	
