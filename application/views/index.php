43b6acb7b2d147ab68343fcd0a9d8c46764f59d3
refojunior <refojunior5@gmail.com>
2018-05-05 07:13:32 +0800

    sedikit tambahan untuk body html

--- application/views/index.php
+++ application/views/index.php
@@ -43,6 +43,30 @@
 			</div>
 		</div>
 	</nav>
+	<nav class="nav-second">
+		<div class="flex-container-second">
+			<div class="container">
+				<ul>
+					<li class="list"><a href="">Semua Kategori</a></li>
+					<li class="list"><a href="">Cat Tembok</a></li>
+					<li class="list"><a href="">Cat Kayu</a></li>
+					<li class="list"><a href="">Cat Besi</a></li>
+					<li class="list"><a href="">Cat Duco</a></li>
+					<li class="list"><a href="">Thinner</a></li>
+					<li class="list"><a href="">Cat Genteng</a></li>
+					<li class="list"><a href="">Cat Waterproofing</a></li>
+					<li class="list"><a href="#">Lain - Lain</a>
+						<div class="dropdown">
+							<a href=""></a>
+							<a href="#">Modacon</a>
+							<a href="#">Link 1</a>
+							<a href="#">Link 1</a>
+						</div>
+					</li>
+				</ul>
+			</div>
+		</div>
+	</nav>
 	<!-- menu mobile -->
 	<ul class="menu-mobile" type="circle">
 		<li><a href="">HOME</a></li>
@@ -68,33 +92,33 @@
 			</div>
 		</li>
 	</ul>
+	
+	<img src="<?= base_url('upload/slideshow/Ramadhan.jpg') ?>" alt="" class="img-fluid img-position" id="img-position">
 
-	<nav class="nav-second">
-		<div class="flex-container-second">
-			<div class="container">
-				<ul>
-					<li class="list"><a href="">Semua Kategori</a></li>
-					<li class="list"><a href="">Cat Tembok</a></li>
-					<li class="list"><a href="">Cat Kayu</a></li>
-					<li class="list"><a href="">Cat Besi</a></li>
-					<li class="list"><a href="">Cat Duco</a></li>
-					<li class="list"><a href="">Thinner</a></li>
-					<li class="list"><a href="">Cat Genteng</a></li>
-					<li class="list"><a href="">Cat Waterproofing</a></li>
-					<li class="list"><a href="#">Lain - Lain</a>
-						<div class="dropdown">
-							<a href=""></a>
-							<a href="#">Modacon</a>
-							<a href="#">Link 1</a>
-							<a href="#">Link 1</a>
-						</div>
-					</li>
-				</ul>
+	<div class="container-fluid">
+		<br>
+		<div class="container">
+			<h1 align="center">ABOUT US</h1>
+			<hr>
+			<div class="row">
+				<div class="col-md-7">
+					<p class="about"><b class="sorot"> CITRA WARNA </b> adalah toko cat terbesar di Bali, yang menyediakan produk-produk berkualitas, dan menjamin harga termurah. Cabang kami tersebar didaerah Denpasar, Nusadua, Ubud, Tabanan, Singaraja dan akan terus berkembang. </p>
+					<a class="btn btn-primary" data-toggle="collapse" href="#collapseExample" role="button" aria-expanded="false" aria-controls="collapseExample">
+					    VISI & MISI 
+					</a>
+					<div class="collapse" id="collapseExample">
+					  <div class="card card-body">
+					    Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident.
+					  </div>
+					</div>
+				</div>
+				<div class="col-md-5">
+					<img src="<?= base_url('assets/img/gif1.gif') ?>" alt="" class="img-fluid">
+				</div>	
 			</div>
-		</div>
-	</nav>
+			
 
-	<div class="container-fluid container-mobile active-mobile">
+		</div>
 	</div>	
 	
 
