23eacfbe057b12b3fa8bfb45a3486db89c67e639
refojunior <refojunior5@gmail.com>
2018-09-01 15:06:05 +0800

    styling inspiration

--- application/views/frontend/inspiration_page.php
+++ application/views/frontend/inspiration_page.php
@@ -1,12 +1,35 @@
-<br>
 
-<div id="wrapper">
-    <div id="myCarousel" data-carousel-3d>
-        <img src="<?= base_url('assets/img/cwa_banner.jpg') ?>" />
-        <img src="<?= base_url('assets/img/kategori.jpg') ?>" selected />
-        <img src="<?= base_url('assets/img/inspiration.jpg') ?>" />
-        <img src="<?= base_url('assets/img/cwa_banner.jpg') ?>" />
-        <img src="<?= base_url('assets/img/kategori.jpg') ?>" selected />
-        <img src="<?= base_url('assets/img/inspiration.jpg') ?>" />
-    </div>
+<div class="container-fluid">
+	<br>
+	<div class="container body-list">
+		<h1>WARNA SOFT</h1>
+		<br>
+		<div id="wrapper">
+		    <div id="myCarousel" data-carousel-3d>
+		        <img src="<?= base_url('assets/img/cwa_banner.jpg') ?>" />
+		        <img src="<?= base_url('assets/img/kategori.jpg') ?>" selected />
+		        <img src="<?= base_url('assets/img/inspiration.jpg') ?>" />
+		        <img src="<?= base_url('assets/img/cwa_banner.jpg') ?>" />
+		        <img src="<?= base_url('assets/img/kategori.jpg') ?>" selected />
+		        <img src="<?= base_url('assets/img/inspiration.jpg') ?>" />
+		    </div>
+		</div>
+	</div>
+	<br>
+	<div class="container body-list">
+		<h1>WARNA SOFT</h1>
+		<br>
+		<div id="wrapper">
+		    <div id="myCarousel" data-carousel-3d>
+		        <img src="<?= base_url('assets/img/cwa_banner.jpg') ?>" />
+		        <img src="<?= base_url('assets/img/kategori.jpg') ?>" selected />
+		        <img src="<?= base_url('assets/img/inspiration.jpg') ?>" />
+		        <img src="<?= base_url('assets/img/cwa_banner.jpg') ?>" />
+		        <img src="<?= base_url('assets/img/kategori.jpg') ?>" selected />
+		        <img src="<?= base_url('assets/img/inspiration.jpg') ?>" />
+		    </div>
+		</div>
+	</div>
 </div>
+
+
